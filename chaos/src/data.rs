19c38075aa6352b676d2f29a02ef82a739399268
Kavika <50282464+KavikaPalletenne@users.noreply.github.com>
2024-02-05 21:00:05 +1100

    fix(chaos): update starter code to be compilable

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -1,19 +1,22 @@
-use axum::response::IntoResponse;
-use axum::Json;
+use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
-
     // Calculate sums and return response
 
+    let response = DataResponse {
+        
+    };
+
+    (StatusCode::OK, Json(response))
 }
 
 #[derive(Deserialize)]
-struct DataRequest {
+pub struct DataRequest {
     // Add any fields here
 }
 
 #[derive(Serialize)]
-struct DataResponse {
+pub struct DataResponse {
     // Add any fields here
 }
--- chaos/src/main.rs
+++ chaos/src/main.rs
@@ -1,15 +1,12 @@
-use axum::http::StatusCode;
-use axum::{response::IntoResponse, routing::post, Json, Router};
-use serde::{Deserialize, Serialize};
+use axum::{routing::post, Router};
 
 mod data;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/process_data", post(data::process_data));
+    let app = Router::new().route("/process_data", post(data::process_data));
 
-    axum::Server::bind("127.0.0.1:3000".parse().unwrap())
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
